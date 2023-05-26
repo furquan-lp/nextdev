90d817a168590887e3021e34730a172d6e323526
Syed F Ahmad <furquan@protonmail.com>
2023-05-26 20:25:21 +0530

    Removed old styling for HomeLogo, changed old colors to the new grayscale colors and added a basic home logo and basic styling for links

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,21 +1,15 @@
 import { Link } from 'react-router-dom';
 
 const HomeLogo = ({ path }) =>
-  <div className="font-titlefont md:text-2xl text-base text-green-light py-2.5 pl-4 transition-colors duration-300
-  hover:text-green-new active:text-white-light select-none">
-    <div className="hidden md:inline">
-      {path ? `/simple dev.site/${path}` : "/simple dev.site"}
-    </div>
-    <div className="inline md:hidden">
-      /simpledev.site
-    </div>
+  <div className="font-logofont md:text-2xl p-2 m-1 text-slate-800 outline-1 border-solid">
+    <b>NEXT</b>DEV.IN // HOME
   </div>;
 
 const TopBarButton = ({ link, linktext, extern, styles }) => {
   const buttonStyle = {
-    link: styles ? `text-green-light ${styles}` : `text-green-light`,
-    li: `inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-blue-gray hover:rounded-md
-     transition-all duration-200`
+    link: styles ? `text-slate-800 ${styles}` : `text-slate-800`,
+    li: `inline md:m-1 m-px md:p-3 p-1 tracking-widest hover:text-white-light hover:bg-slate-600 transition-all
+     duration-200`
   };
   if (extern) {
     return <a href={link} className={buttonStyle.link}>
@@ -33,8 +27,8 @@ const TopBarButton = ({ link, linktext, extern, styles }) => {
 };
 
 const TopBar = ({ page }) =>
-  <div className="flex flex-wrap md:flex-nowrap justify-between bg-green-dark text-white pb-2 md:p-1 md:m-1.5 m-px
-   rounded-md">
+  <div className="flex flex-wrap md:flex-nowrap justify-between text-slate-800 pb-2 md:p-1 md:m-1.5 m-px
+   rounded-md ">
     <Link to="/"><HomeLogo path={page} /></Link>
     <ul className="flex items-center list-none text-sm md:text-base md:m-1 p-0">
       <TopBarButton link="/" linktext="Home" styles="hidden md:inline" />
