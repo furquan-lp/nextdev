39ce53185f3464cb8e83894c4056e6ff159eb75e
Syed F Ahmad <furquan@protonmail.com>
2023-05-26 22:31:50 +0530

    Remove almost all old components and their code, and commented out whatever remained that wasn't imported and was thus causing errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,18 @@
 import preval from 'preval.macro';
 import { SiReact, SiTailwindcss } from 'react-icons/si';
 import { MdOutlineViewCarousel } from 'react-icons/md';
-import { FiExternalLink } from 'react-icons/fi';
-import { IconContext } from "react-icons";
 
 import TopBar from '../components/TopBar';
-import Welcome from '../components/Welcome';
-import HiCards from '../components/Hi';
-import Footer from '../components/Footer';
-
-const TechLinkButton = ({ link, linktext, children }) =>
-  <a href={link} className="flex items-center ml-2 my-1 md:ml-4 md:my-2 mr-auto text-lg md:text-2xl underline
-   md:no-underline p-1.5 rounded bg-blue-gray/50 hover:text-green-light active:text-white hover:bg-transparent
-    md:hover:underline transition-all duration-200">
-    {children}
-    {linktext}
-    <IconContext.Provider value={{ size: "0.8em" }}>
-      <FiExternalLink className="ml-0.5 md:h-2/3" />
-    </IconContext.Provider>
-  </a>;
 
 const Home = () => {
   return (
-    <div className="bg-mountains-blur bg-center bg-cover bg-no-repeat">
+    <div className="bg-center bg-cover bg-no-repeat">
       <TopBar />
       <div className="flex flex-col md:mx-12">
-        <Welcome />
-        <HiCards />
-        <div className="font-sans font-bold text-white md:text-2xl text-lg mx-4 lg:p-10 p-4 ">This website
+        <div className="font-sans font-bold text-slate-600 md:text-2xl text-lg mx-4 lg:p-10 p-4 ">This website
           is built using:
           <div className="flex flex-col mb-5 font-normal">
-            <TechLinkButton link="https://reactjs.org/" linktext="ReactJS">
+            {/*<TechLinkButton link="https://reactjs.org/" linktext="ReactJS">
               <SiReact className="mr-1" />
             </TechLinkButton>
             <TechLinkButton link="https://tailwindcss.com/" linktext="Tailwind CSS">
@@ -38,7 +20,7 @@ const Home = () => {
             </TechLinkButton>
             <TechLinkButton link="https://flowbite.com/" linktext="Flowbite">
               <MdOutlineViewCarousel className="mr-1" />
-            </TechLinkButton>
+  </TechLinkButton>*/}
           </div>
           Check the source code <a
             href="https://github.com/furquan-lp/simpledev.site/" className="underline hover:text-green-light">here</a>.
@@ -47,7 +29,7 @@ const Home = () => {
           <p>Build {preval`module.exports = new Date().toISOString();`}</p>
         </div>
       </div>
-      <Footer />
+      {/*<Footer />*/}
     </div>
   );
 };
