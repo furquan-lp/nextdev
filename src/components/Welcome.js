cfcdbf489ed05765c5e6ef1bdea7cf247fa46109
Syed F Ahmad <furquan@protonmail.com>
2023-05-31 19:47:38 +0530

    Added a transparent white background and the text to CarouselCard, added max width to CarouselCard content and trimmed down the text for CarouselCards in MyCarousel

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,12 +8,15 @@ import iotImg from '../assets/device.gif';
 import githubImg from '../assets/github-screenshot.webp';
 
 const CarouselCard = ({ title, tags, text, buttonLink, buttonText, image }) =>
-  <div className="flex mx-px my-px flex-wrap md:flex-nowrap">
+  <div className="flex mx-px my-px flex-wrap md:flex-nowrap bg-white/80">
     <img className="md:w-6/12 w-full md:h-96 h-auto" src={image}
       alt={`${title} screenshot"`} />
-    <div className="flex flex-col m-4">
-      <span className="font-cardtitle text-xl my-2 font-bold">{tags.map(e => <span className="m-1 text-red-600">#{e}</span>)}</span>
-      <span className="font-cardtitle text-4xl">{title}</span>
+    <div className="flex flex-col m-4 max-w-3xl">
+      <span className="font-cardtitle text-xl my-2 font-bold">
+        {tags.map(e => <span className="m-1 text-red-600">#{e}</span>)}
+      </span>
+      <span className="font-cardtitle text-3xl md:text-5xl">{title}</span>
+      <span className="text-xl md:text-3xl mt-5 font-light">{text}</span>
     </div>
   </div>;
 
@@ -80,27 +83,22 @@ const MyCarousel = () => {
       <CarouselCard title="The Chip Tinker Project - A Gatsby Blog"
         tags={["web", "gatsby", "react"]}
         text="TCTP.xyz (The Chip Tinker Project) is my blog written using Gatsby and Tailwind CSS, and hosted on a
-         Netlify backend. Thanks to Tailwind CSS it has a mobile first design and it uses Netlify CMS which helps me
-         push out blog posts without having to mess with the internal Markdown."
+         Netlify backend."
         buttonLink="https://tctp.xyz"
         buttonText="Visit site"
         image={tctpImg}
       />
       <CarouselCard title="Webenv - A React Native App on an Iot Backend"
         tags={["react", "mobile", "IoT"]}
-        text="WebEnv is a React Native app which connects to an IoT backend to monitor and log environment data.
-         Available for Android, iOS and the web. The IoT backend is powered by an ESP32 microcontroller which exposes a
-         JSON API over an HTTP server (hosted on the microcontroller itself!)."
+        text="WebEnv is a React Native app which connects to an IoT backend to monitor and log environment data."
         buttonLink="http://webenv.simpledev.site"
         buttonText="Check out the live demo"
         image={webenvImg}
       />
       <CarouselCard title="Arduino, Microcontrollers and Other Iot Stuff"
         tags={["arduino", "IoT", "backend"]}
-        text="Two years ago I was working on a project to log environmental data (air quality, temperature, humidity,
-         etc.) and display it to an LCD screen. Later on I ported the code over to an ESP32 board which served the
-         data over LAN WiFi called 'envmon'. A minimal version which forwarded the data over the Internet to a JSON
-         database was used as the backend for WebEnv."
+        text="Two years ago I was working on a project to log environmental data and display it to an LCD screen. Later
+         on I ported the code over to an ESP32 board which was used as the backend for WebEnv."
         buttonLink="https://github.com/furquan-lp/environment-monitor"
         buttonText="Check out the source code"
         image={iotImg}
