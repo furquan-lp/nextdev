daf567b454d65ca80df6476577de85e72f8c9773
Syed F Ahmad <furquan@protonmail.com>
2023-05-31 22:09:39 +0530

    Added the clip-path classes and the shadow container to the two parts of the component

--- src/components/HiBox.js
+++ src/components/HiBox.js
@@ -1,13 +1,18 @@
-
 const HiBox = () =>
   <div className="flex flex-wrap md:flex-nowrap justify-around text-slate-800">
-    <div className="flex flex-col text-2xl md:text-6xl my-5 mr-2 px-24 py-10 flex-grow font-aboutfont tracking-wide bg-white/90 shadow-lg">
-      <span className="my-1">Hi,</span>
-      <span className="my-1">I'm Syed</span>
-      <span className="my-1">Web Developer</span>
+    <div className="clipped-shadow flex-grow">
+      <div className="flex flex-col text-3xl md:text-6xl my-5 px-24 py-10 flex-grow font-aboutfont tracking-wide
+       bg-white/90 clipped-card-right">
+        <span className="my-1">Hi,</span>
+        <span className="my-1">I'm Syed</span>
+        <span className="my-1">Web Developer</span>
+      </div>
     </div>
-    <div className="flex flex-col text-xl md:text-4xl my-5 px-24 py-10 flex-grow font-cardtitle items-center bg-white/90 shadow-lg">
-      About Me
+    <div className="clipped-shadow flex-grow">
+      <div className="flex flex-col text-2xl md:text-5xl my-5 px-24 py-10 flex-grow font-cardtitle items-center
+       bg-white/90 clipped-card-left">
+        <i>About Me</i>
+      </div>
     </div>
   </div>;
 
