16f90eb096c6e687b8b8f47ad2f63eed38facd5b
Syed F Ahmad <furquan@protonmail.com>
2023-05-31 21:24:12 +0530

    Added a basic 'HiBox' component with some basic styling to replace the older Hi.js

---
+++ src/components/HiBox.js
@@ -0,0 +1,14 @@
+
+const HiBox = () =>
+  <div className="flex flex-wrap md:flex-nowrap justify-around text-slate-800">
+    <div className="flex flex-col text-2xl md:text-6xl my-5 mr-2 px-24 py-10 flex-grow font-aboutfont tracking-wide bg-white/90 shadow-lg">
+      <span className="my-1">Hi,</span>
+      <span className="my-1">I'm Syed</span>
+      <span className="my-1">Web Developer</span>
+    </div>
+    <div className="flex flex-col text-xl md:text-4xl my-5 px-24 py-10 flex-grow font-cardtitle items-center bg-white/90 shadow-lg">
+      About Me
+    </div>
+  </div>;
+
+export default HiBox;
