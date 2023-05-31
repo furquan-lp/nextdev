7ae046f172c172f0198404b952964594faad5a9d
Syed F Ahmad <furquan@protonmail.com>
2023-05-31 22:17:58 +0530

    Added the resume link button with FiArrowRight icon to the first part of HiBox

--- src/components/HiBox.js
+++ src/components/HiBox.js
@@ -1,3 +1,5 @@
+import { FiArrowRight } from 'react-icons/fi';
+
 const HiBox = () =>
   <div className="flex flex-wrap md:flex-nowrap justify-around text-slate-800">
     <div className="clipped-shadow flex-grow">
@@ -6,6 +8,9 @@ const HiBox = () =>
         <span className="my-1">Hi,</span>
         <span className="my-1">I'm Syed</span>
         <span className="my-1">Web Developer</span>
+        <a href="/resume-latest.pdf" className="flex items-center mt-2 mr-auto p-1 text-xl md:text-2xl border
+         border-slate-400 bg-slate-200 hover:underline hover:text-white hover:bg-slate-600 active:text-white
+          active:bg-slate-500 transition-all duration-200">Résumé <FiArrowRight /></a>
       </div>
     </div>
     <div className="clipped-shadow flex-grow">
