23d84abea6f38b33d88b0eb3ca4c5e69e0572f2b
Syed F Ahmad <furquan@protonmail.com>
2023-05-31 22:33:13 +0530

    Added responsive padding and margin, and additional shadow for screens smaller than md (where the containers aren't clipped and don't use clipped shadow)

--- src/components/HiBox.js
+++ src/components/HiBox.js
@@ -2,20 +2,20 @@ import { FiArrowRight } from 'react-icons/fi';
 
 const HiBox = () =>
   <div className="flex flex-wrap md:flex-nowrap justify-around text-slate-800">
-    <div className="clipped-shadow flex-grow">
-      <div className="flex flex-col text-3xl md:text-6xl my-5 px-24 py-10 flex-grow font-aboutfont tracking-wide
-       bg-white/90 clipped-card-right">
+    <div className="clipped-shadow flex-grow shadow-lg md:shadow-none my-2 md:m-0">
+      <div className="flex flex-col text-4xl md:text-6xl my-2 px-8 py-4 md:my-5 md:px-24 md:py-10 flex-grow
+       font-aboutfont tracking-wide bg-white/90 clipped-card-right">
         <span className="my-1">Hi,</span>
         <span className="my-1">I'm Syed</span>
         <span className="my-1">Web Developer</span>
-        <a href="/resume-latest.pdf" className="flex items-center mt-2 mr-auto p-1 text-xl md:text-2xl border
+        <a href="/resume-latest.pdf" className="flex items-center mt-5 mr-auto p-1 text-xl md:text-2xl border
          border-slate-400 bg-slate-200 hover:underline hover:text-white hover:bg-slate-600 active:text-white
           active:bg-slate-500 transition-all duration-200">Résumé <FiArrowRight /></a>
       </div>
     </div>
-    <div className="clipped-shadow flex-grow">
-      <div className="flex flex-col text-2xl md:text-5xl my-5 px-24 py-10 flex-grow font-cardtitle items-center
-       bg-white/90 clipped-card-left">
+    <div className="clipped-shadow flex-grow shadow-lg md:shadow-none my-2 md:m-0">
+      <div className="flex flex-col text-3xl md:text-5xl my-2 px-8 py-4 md:my-5 md:px-24 md:py-10 flex-grow
+       font-cardtitle items-center bg-white/90 clipped-card-left">
         <i>About Me</i>
       </div>
     </div>
