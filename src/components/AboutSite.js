6a0890cc28ce4c37b86032a31020ebffb482ba7d
Syed F Ahmad <furquan@protonmail.com>
2023-06-01 13:20:39 +0530

    Added an unordered list of link buttons showing the tech used

--- src/components/AboutSite.js
+++ src/components/AboutSite.js
@@ -1,22 +1,21 @@
 import preval from 'preval.macro';
-import { SiReact, SiTailwindcss } from 'react-icons/si';
+import { SiReact, SiTailwindcss, SiMaterialdesign } from 'react-icons/si';
 import { MdOutlineViewCarousel } from 'react-icons/md';
 
 const AboutSite = () =>
   <details className="font-aboutmono font-bold text-slate-600 md:text-2xl text-lg my-4 md:my-6 ">
     <summary className="">This website is built using:</summary>
-    <div className="flex flex-col mb-5 font-normal open:font-bold">
-      ??
-      {/*<TechLinkButton link="https://reactjs.org/" linktext="ReactJS">
-      <SiReact className="mr-1" />
-    </TechLinkButton>
-    <TechLinkButton link="https://tailwindcss.com/" linktext="Tailwind CSS">
-      <SiTailwindcss className="mr-1" />
-    </TechLinkButton>
-    <TechLinkButton link="https://flowbite.com/" linktext="Flowbite">
-      <MdOutlineViewCarousel className="mr-1" />
-</TechLinkButton>*/}
-    </div>
+    <ul className="flex flex-col mb-5 font-normal open:font-bold md:text-xl text-base">
+      <li className="mr-auto"><a href="https://reactjs.org/" className="flex items-center mx-1 my-2 p-1 border
+       border-blue-react hover:bg-blue-react hover:text-white hover:underline transition-all duration-200">
+        <SiReact className="mr-1" />ReactJS</a></li>
+      <li className="mr-auto"><a href="https://tailwindcss.com/" className="flex items-center mx-1 my-2 p-1 border
+       border-blue-tailwind hover:bg-blue-tailwind hover:text-white hover:underline transition-all duration-200">
+        <SiTailwindcss className="mr-1" />Tailwind CSS</a></li>
+      <li className="mr-auto"><a href="https://www.material-tailwind.com/" className="flex items-center mx-1 my-2 p-1
+       border border-blue-mui hover:bg-blue-mui hover:text-white hover:underline transition-all duration-200">
+        <SiMaterialdesign className="mr-1" />Material Tailwind</a></li>
+    </ul>
     Check the source code <a
       href="https://github.com/furquan-lp/simpledev.site/" className="underline hover:text-blue-500">here</a>.
     Report bugs <a href="https://github.com/furquan-lp/simpledev.site/issues"
