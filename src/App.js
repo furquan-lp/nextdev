14958eafc7da78978dc00dc4069bd1b19edca4be
Syed F Ahmad <furquan@protonmail.com>
2023-06-02 00:00:46 +0530

    Added a route for Contact

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import Home from './pages/Home';
 import { About } from './pages/About';
 import Portfolio from './pages/Portfolio';
+import { Contact } from './pages/Contact';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
