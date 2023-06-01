38b861ae88e656d192918133591a6e6a7bee01ef
Syed F Ahmad <furquan@protonmail.com>
2023-06-02 00:00:27 +0530

    Added the Contact page (blank)

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import TopBar from '../components/TopBar';
+import Footer from '../components/Footer';
+
+export const Contact = () =>
+  <div className="bg-minimal-react bg-contain bg-center bg-no-repeat ">
+    <TopBar page="home" highlight={[false, false, false, false, true]} />
+    <div className="flex flex-col md:mx-24">
+    </div>
+    <Footer />
+  </div>;
