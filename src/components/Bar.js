328b7bffb85f6c60288822dfddb3b1e221d6b239
Syed F Ahmad <furquan@protonmail.com>
2023-06-01 10:46:37 +0530

    Added the colored Bar separator component

---
+++ src/components/Bar.js
@@ -0,0 +1,11 @@
+const Bar = () =>
+  <div className="flex flex-row-reverse mx-1 my-0.5">
+    <span className="py-0.5 px-4 bg-red-300"></span>
+    <span className="py-0.5 px-4 bg-blue-300"></span>
+    <span className="py-0.5 px-4 bg-amber-300"></span>
+    <span className="py-0.5 px-4 bg-green-300"></span>
+    <span className="py-0.5 px-4 bg-purple-300"></span>
+    <span className="flex-grow p-0.5 bg-slate-300/50"></span>
+  </div>;
+
+export default Bar;
