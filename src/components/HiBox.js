73e0a373433269211f0bd075195bbc6f3b901388
Syed F Ahmad <furquan@protonmail.com>
2023-06-01 10:46:02 +0530

    Decreased background transprarency

--- src/components/HiBox.js
+++ src/components/HiBox.js
@@ -4,7 +4,7 @@ import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
 const HiBox = () =>
   <div className="flex flex-wrap md:flex-nowrap justify-around text-slate-800">
     <div className="flex flex-col text-4xl md:text-6xl my-2 mx-1 px-8 py-4 md:my-5 md:mr-5 md:px-24 md:py-10 flex-grow
-       font-aboutfont tracking-wide bg-white/90 shadow-lg border border-slate-100">
+       font-aboutfont tracking-wide bg-white/95 shadow-lg border border-slate-100">
       <span className="my-1">Hi,</span>
       <span className="my-1">I'm Syed</span>
       <span className="my-1">Web Developer</span>
@@ -13,7 +13,7 @@ const HiBox = () =>
           active:bg-slate-500 transition-all duration-200">Résumé <FiArrowRight /></a>
     </div>
     <div className="flex flex-col text-3xl md:text-5xl my-2 mx-1 px-8 py-4 md:my-5 md:ml-5 md:px-20 md:py-10 flex-grow
-       font-cardtitle bg-white/90 shadow-lg border border-slate-100">
+       font-cardtitle bg-white/95 shadow-lg border border-slate-100">
       <i className="flex mb-4 self-center"><MdKeyboardDoubleArrowRight />About Me</i>
       <div className="flex justify-center font-aboutmono text-teal-600 text-3xl typewriter ">//---Coder---//</div>
       <div className="flex justify-center font-aboutmono text-orange-600 text-3xl typewriter ">//--Tinkerer--//</div>
