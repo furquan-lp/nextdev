69637df9aa2fb9ae481c3d5d4cfd92d0f6cb0332
Syed F Ahmad <furquan@protonmail.com>
2023-06-09 20:00:58 +0530

    Added the notfound image, message and a link with an icon back to home

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
+import { FiArrowUpCircle } from 'react-icons/fi';
+
 import TopBar from '../components/TopBar';
 import Footer from '../components/Footer';
 
 export const NotFound = () =>
   <div className="bg-minimal-react bg-contain bg-center bg-no-repeat min-h-screen">
     <TopBar page="home" highlight={[false, false, false, false, false]} />
-    <div className="flex flex-col md:mx-24">
-      404 Not Found
+    <div className="flex flex-col items-center text-slate-700 mx-1 md:mx-0">
+      <img src="./notfound.webp" alt="Empty cardboard boxes with the word 'oops'" className="w-1/2 border my-6" />
+      <span className="my-2 md:my-6 font-bold text-2xl md:text-4xl font-aboutmono">Uh oh! The page you were looking for
+        doesn't exist.</span>
+      <Link to="/" className="flex text-xl md:text-3xl"><FiArrowUpCircle className="self-center mr-2" />Back to home.
+      </Link>
     </div>
     <div className="sticky top-[100vh]"><Footer /></div>
   </div>;
