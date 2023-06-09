9340236455abe9eaf70830888b03d8a77bebae3a
Syed F Ahmad <furquan@protonmail.com>
2023-06-10 01:24:01 +0530

    Added responsive margin and sizes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,7 +10,7 @@ const PortfolioCardImage = ({ image, alt }) =>
   <img src={image} alt={alt} className="p-1 h-1/2 md:w-1/3 md:h-80 bg-white/90 border shadow-lg" />;
 
 const PortfolioCardTag = ({ text, color, children }) =>
-  <div className={`flex p-1.5 mr-auto rounded md:text-lg ${color}`}>
+  <div className={`flex p-1.5 mr-auto rounded my-2 md:my-0 md:text-lg ${color}`}>
     {children}
     <span className="ml-1">{text}</span>
   </div>;
@@ -19,8 +19,8 @@ const PortfolioCardText = ({ title, text, tags, code, link, margin }) =>
   <div className={`flex flex-col max-h-fit justify-around grow p-4 text-slate-700 bg-white/90 border shadow-lg
    ${margin}`}>
     <span className="flex flex-col">
-      <span className="my-2 md:my-6 font-abouttext text-4xl">{title}</span>
-      <span className="my-2 text-xl">{text}</span>
+      <span className="my-2 md:my-6 font-abouttext text-3xl md:text-4xl">{title}</span>
+      <span className="my-2 text-lg md:text-xl">{text}</span>
     </span>
     <PortfolioCardTag text="React" color="bg-blue-react text-white">
       <SiReact className="self-center" />
@@ -38,13 +38,15 @@ const PortfolioCardText = ({ title, text, tags, code, link, margin }) =>
 const PortfolioCard = ({ image, title, text, tags, codeLink, siteLink, reverse }) => {
   if (reverse) {
     return <div className="flex flex-col md:flex-row">
-      <PortfolioCardText title={title} text={text} tags={tags} code={codeLink} link={siteLink} margin="md:mr-1" />
+      <PortfolioCardText title={title} text={text} tags={tags} code={codeLink} link={siteLink}
+        margin="mb-1 md:mb-0 md:mr-1" />
       <PortfolioCardImage image={image} alt={title} />
     </div>;
   } else {
     return <div className="flex flex-col md:flex-row">
       <PortfolioCardImage image={image} alt={title} />
-      <PortfolioCardText title={title} text={text} tags={tags} code={codeLink} link={siteLink} margin="md:ml-1" />
+      <PortfolioCardText title={title} text={text} tags={tags} code={codeLink} link={siteLink}
+        margin="mt-1 md:mt-0 md:ml-1" />
     </div>;
   }
 };
@@ -52,8 +54,8 @@ const PortfolioCard = ({ image, title, text, tags, codeLink, siteLink, reverse }
 export const Portfolio = () =>
   <div className="bg-minimal-react bg-contain bg-center bg-no-repeat min-h-screen">
     <TopBar page="portfolio" highlight={[false, false, false, true, false]} />
-    <div className="flex flex-col md:mx-24">
-      <span className="font-aboutfont text-4xl md:text-5xl my-2 md:my-6">Stuff I've built (and have been working on)</span>
+    <div className="flex flex-col mx-1 md:mx-24">
+      <span className="font-aboutfont text-4xl md:text-5xl my-4 md:my-6">Stuff I've built (and have been working on)</span>
       <PortfolioCard image={testImg} title="SimpleDev.site" text="SimpleDev.site is my portfolio website built with
        React. You are HERE." codeLink="https://github.com/furquan-lp/" siteLink="https://oldsite.nextdev.in/" />
     </div>
