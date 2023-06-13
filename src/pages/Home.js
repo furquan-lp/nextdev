be3bd44ae341805108b7ef5f6f320e69e78b88b4
Syed F Ahmad <furquan@protonmail.com>
2023-06-13 19:52:13 +0530

    Added state to store the carousel data and used a useEffect to fetch it from the backend API using initCarousel, and passed the data along to WelcomeCarousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import TopBar from '../components/TopBar';
 import WelcomeCarousel from '../components/Welcome';
 import HiBox from '../components/HiBox';
@@ -6,14 +8,19 @@ import Footer from '../components/Footer';
 import AboutSite from '../components/AboutSite';
 
 import { usePageTitle } from '../myHooks';
+import service from '../services';
 
 const Home = () => {
   usePageTitle('Home');
+  const [carousel, setCarousel] = useState();
+
+  useEffect(() => { service.initCarousel(setCarousel) }, []);
+
   return (
     <div className="bg-minimal-react bg-contain bg-center bg-no-repeat min-h-screen">
       <TopBar page="home" highlight={[true, false, false, false, false]} />
       <div className="flex flex-col md:mx-24">
-        <WelcomeCarousel />
+        <WelcomeCarousel carouselData={carousel} apiURL={service.API} />
         <Bar />
         <HiBox />
         <Bar />
