46a5f404eba9ef9a2f46b5b0400be17bcbd7a5c3
Syed F Ahmad <furquan@protonmail.com>
2023-06-13 19:56:10 +0530

    Removed the logging statement

--- src/services.js
+++ src/services.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const API = process.env.REACT_APP_API_URL;
 
 const initCarousel = (setCarousel) => {
-  console.log('API is ', API)
   axios.get(API + 'db/carousel').then(response => setCarousel(response.data));
 };
 
