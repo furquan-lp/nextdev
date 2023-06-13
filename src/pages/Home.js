71ef6da40d8d00048b78c66174f607199c77c569
Syed F Ahmad <furquan@protonmail.com>
2023-06-13 20:01:33 +0530

    Moved carousel data fetching and state handling code to App so the data is only fetched once

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 import TopBar from '../components/TopBar';
 import WelcomeCarousel from '../components/Welcome';
 import HiBox from '../components/HiBox';
@@ -8,19 +6,15 @@ import Footer from '../components/Footer';
 import AboutSite from '../components/AboutSite';
 
 import { usePageTitle } from '../myHooks';
-import service from '../services';
 
-const Home = () => {
+const Home = ({ carouselData, apiURL }) => {
   usePageTitle('Home');
-  const [carousel, setCarousel] = useState();
-
-  useEffect(() => { service.initCarousel(setCarousel) }, []);
 
   return (
     <div className="bg-minimal-react bg-contain bg-center bg-no-repeat min-h-screen">
       <TopBar page="home" highlight={[true, false, false, false, false]} />
       <div className="flex flex-col md:mx-24">
-        <WelcomeCarousel carouselData={carousel} apiURL={service.API} />
+        <WelcomeCarousel carouselData={carouselData} apiURL={apiURL} />
         <Bar />
         <HiBox />
         <Bar />
