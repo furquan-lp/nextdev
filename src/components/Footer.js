40f09b2f2a0dd84313fabfe969b93380371391d2
Syed F Ahmad <furquan@protonmail.com>
2023-06-15 23:42:18 +0530

    Changed 'GitHub Pages' to 'Vercel, Railway and MongoDB Atlas'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { FiGithub } from 'react-icons/fi';
 const Footer = () => <div className="flex justify-between items-center bg-white text-sm md:text-base
 text-slate-600 p-1.5 border border-slate-300 sticky top-[100vh]">
   <div>
-    <span className="hidden md:inline">Powered by GitHub Pages.</span> <a className="underline decoration-slate-600
+    <span className="hidden md:inline">Powered by Vercel, Railway and MongoDB Atlas.</span> <a className="underline decoration-slate-600
     md:decoration-transparent md:hover:decoration-slate-600 transition-all duration-200"
       href="https://github.com/furquan-lp/nextdev/blob/master/LICENSE">
       Copyright &#169; 2023 Syed Furquan Ahmad.</a>
