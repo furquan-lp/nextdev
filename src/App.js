d7a4c0db2228f73c8f31f493be093397f74518d8
Syed F Ahmad <furquan@protonmail.com>
2023-06-16 11:11:03 +0530

    Added a portfolio state to store the fetched portfolio data from initPortfolio and passed it along to the Portfolio page

--- src/App.js
+++ src/App.js
@@ -14,19 +14,23 @@ import service from './services';
 
 const App = () => {
   const [carousel, setCarousel] = useState();
+  const [portfolio, setPortfolio] = useState();
   const [backendVersion, setBackendVersion] = useState({ version: "unknown" });
 
   useEffect(() => {
     service.initCarousel(setCarousel);
+    service.initPortfolio(setPortfolio);
     service.initBackendVersion(setBackendVersion);
   }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home carouselData={carousel} apiURL={service.API} backendVersion={backendVersion} />} />
         <Route path="*" element={<NotFound backendVersion={backendVersion} />} />
         <Route path="about" element={<About backendVersion={backendVersion} />} />
-        <Route path="portfolio" element={<Portfolio backendVersion={backendVersion} />} />
+        <Route path="portfolio" element={<Portfolio portfolio={portfolio} apiURL={service.API}
+          backendVersion={backendVersion} />} />
         <Route path="contact" element={<Contact backendVersion={backendVersion} />} />
         <Route path="blog" element={<BlogComingSoon />} />
       </Routes>
