5c160b2074f6347898e870ffa40e2d523ee80aab
Syed F Ahmad <furquan@protonmail.com>
2023-06-21 00:11:44 +0530

    Added postMessage function to send a message to the mail endpoint of the backend

--- src/services.js
+++ src/services.js
@@ -13,4 +13,7 @@ const initPortfolio = (setPortfolio) => {
 const initBackendVersion = (setBackendVersion) =>
   axios.get(API + 'version').then(response => setBackendVersion(response.data));
 
-export default { initCarousel, initPortfolio, initBackendVersion, API };
+const postMessage = (message) =>
+  axios.post(API + 'mail/send', message).catch(err => console.debug('Error while sending message: ', err));
+
+export default { initCarousel, initPortfolio, initBackendVersion, postMessage, API };
