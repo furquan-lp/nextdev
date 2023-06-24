94e5731e8dd6c6226401ded219aa4bb7d28c4c13
Syed F Ahmad <furquan@protonmail.com>
2023-06-25 00:17:20 +0530

    Added Google Analytics using the GA_TAG with GA4React

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import GA4React from "ga-4-react";
 import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,3 +14,12 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+try {
+  setTimeout(() => {
+    const ga4react = new GA4React(process.env.REACT_APP_GA_TAG);
+    ga4react.initialize().catch(err => console.error(err));
+  }, 4000);
+} catch (err) {
+  console.error(err);
+}
