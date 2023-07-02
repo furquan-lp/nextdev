c089a750d71d3b6b6e8fa4bf147752ad0113fd2a
Syed F Ahmad <furquan@protonmail.com>
2023-07-02 20:41:40 +0530

    Added horizontal padding to the icon card text

--- src/components/AboutSite.js
+++ src/components/AboutSite.js
@@ -10,18 +10,18 @@ const AboutSite = () =>
         <a href="https://reactjs.org/" className="group flex flex-col border border-blue-react text-blue-react
          items-center m-1 md:mx-2 hover:bg-blue-react hover:text-white active:text-white active:bg-blue-react
           transition-all duration-200 "><SiReact className="text-6xl m-2" /><span className="border-t
-           border-blue-react group-hover:border-white mt-1 p-2 text-xl hover:underline">ReactJS</span>
+           border-blue-react group-hover:border-white mt-1 p-2 px-4 text-xl hover:underline">ReactJS</span>
         </a>
         <a href="https://tailwindcss.com/" className="group flex flex-col border border-blue-tailwind
          text-blue-tailwind items-center m-1 md:mx-2 hover:bg-blue-tailwind hover:text-white active:text-white
           active:bg-blue-tailwind transition-all duration-200"><SiTailwindcss className="text-6xl m-2" />
-          <span className="border-t border-blue-tailwind group-hover:border-white mt-1 p-2 text-xl hover:underline">
-            Tailwind</span>
+          <span className="border-t border-blue-tailwind group-hover:border-white mt-1 p-2 px-3 text-xl
+           hover:underline">Tailwind</span>
         </a>
         <a href="https://www.material-tailwind.com/" className="group flex flex-col border border-blue-mui
          text-blue-mui items-center m-1 md:mx-2 hover:bg-blue-mui hover:text-white active:text-white
           active:bg-blue-mui transition-all duration-200"><SiMaterialdesign className="text-6xl m-2" />
-          <span className="border-t border-blue-mui group-hover:border-white mt-1 p-2 text-xl hover:underline">
+          <span className="border-t border-blue-mui group-hover:border-white mt-1 p-2 px-3 text-xl hover:underline">
             Material<br />Tailwind</span>
         </a>
       </div>
