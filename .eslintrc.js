b6d82c1bddb22e81c5b53fb012c031ec401cba87
Syed F Ahmad <furquan@protonmail.com>
2023-08-08 17:56:59 +0530

    Added some basic rules and turned off prop-types, react-in-jsx-scope and no-unescaped-entities

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,24 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "react/no-unescaped-entities": "off",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ]
     }
 }
