24c10ead0162ec86ad6244bd9a8455e29f99b7d9
Syed F Ahmad <furquan@protonmail.com>
2023-08-08 16:40:55 +0530

    Added a backend prop for the backend name in the footer (showing nothing in its place if it's undefined)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import { Tooltip } from '@material-tailwind/react';
 import { FiGithub } from 'react-icons/fi';
 
-const Footer = ({ version }) => <div className="flex justify-between items-center bg-white text-sm md:text-base
+const Footer = ({ version, backend }) => <div className="flex justify-between items-center bg-white text-sm md:text-base
 text-slate-600 p-1.5 border border-slate-300 sticky top-[100vh]">
   <span>
-    <span className="hidden md:inline">Powered by Vercel, Railway and MongoDB Atlas. </span>
+    <span className="hidden md:inline">Powered by Vercel{backend && <>, {backend}</>} and MongoDB Atlas. </span>
     <a className="md:underline decoration-slate-600 md:decoration-transparent md:hover:decoration-slate-600
      transition-all duration-200" href="https://github.com/furquan-lp/nextdev/blob/master/LICENSE">
       Copyright &#169; 2023 Syed Furquan Ahmad.</a>
